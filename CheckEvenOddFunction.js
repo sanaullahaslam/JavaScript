4b1e03ec3830c00af2e9752e90c8aca7bb1494e3
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-09-20 07:28:47 -0700

    Parameter function program in which we pass parameter then function check it is even or odd

---
+++ CheckEvenOddFunction.js
@@ -0,0 +1,14 @@
+function chkevenodd(num) {
+
+    if(num%2==0)    {
+        console.log("even")
+    }
+    else{
+        console.log("odd");
+    }
+        
+        
+    }
+    
+    
+    let result=chkevenodd(75);
