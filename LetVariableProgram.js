11cd2984775554b73867b094017d935bf53834bd
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-09-20 07:15:38 -0700

    Let Variable Program

---
+++ LetVariableProgram.js
@@ -0,0 +1,11 @@
+//let variable replaced by var after 2015 or 2016 update
+//let variable we can modify value later 
+
+
+let name="Sanuallah"
+
+console.log(name)
+
+name="Aslam"
+
+console.log(name)
