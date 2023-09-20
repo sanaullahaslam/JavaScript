4de8cbc83247a3be83e4999e2f58af84a6ab27f7
sanaullahaslam <sanaullahaslam09@gmail.com>
2023-09-20 07:06:26 -0700

    This is simple DataTypes Program in JavaScript

---
+++ DataTypesProgram.js
@@ -0,0 +1,39 @@
+let num=3155;
+
+console.log("Integer value = "+num);
+
+let condt=8>5;
+
+console.log("8 is greater 5 = :",condt)
+
+let name="IBA STUDENT";
+
+console.log("String = "+name);
+
+const d= new Date();
+
+console.log(d);
+
+let und;
+
+console.log("undefined = "+und)
+
+const intarray = [3,5,7,9];
+
+console.log("integer array "+intarray)
+
+const strarray = ["Sanaullah " , " Asad " , " Ali " , " Ramim"];
+
+console.log("String array "+strarray)
+
+const mixarray = ["Sanuallah ", 9, "Ronaldo ",7];
+
+console.log("mixed array "+mixarray)
+
+let data = {
+  name : "Sanaullah",
+ sec : 'B',
+  semester : 5
+};
+
+console.log(data)
